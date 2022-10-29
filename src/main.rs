baf60880c26aa9788740212a2e4919c15b62351b
Brian Lachniet <blachniet@gmail.com>
2022-10-29 07:59:13 -0400

    Fix error counting running durations twice

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let mut latest_entries = client.get_latest_entries()?;
     latest_entries.sort_unstable_by_key(|e| e.start);
 
+    let mut is_running = false;
     let mut dur_today = Duration::zero();
     for entry in latest_entries
         .iter()
@@ -31,17 +32,15 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         }) {
         println!("{}", fmt_entry(entry));
         dur_today = dur_today + entry.duration;
+        is_running = is_running || entry.is_running;
     }
 
-    if let Some(current_entry) = client.get_current_entry()? {
-        dur_today = dur_today + current_entry.duration;
-        fmt_entry(&current_entry);
-    } else {
+    if !is_running {
         println!("🤷 No timers running");
     }
 
     let dur_today = fmt_duration(dur_today);
-    println!("\n⏱ {dur_today} logged today");
+    println!("\n⏱  {dur_today} logged today");
 
     Ok(())
 }
@@ -76,7 +75,7 @@ fn get_api_token() -> Result<String, Box<dyn error::Error>> {
 fn fmt_entry(entry: &TimeEntry) -> String {
     let icon = match entry.is_running {
         true => "🏃",
-        false => "-",
+        false => "- ",
     };
     let duration = fmt_duration(entry.duration);
     let project_name = entry.project_name.as_ref().map_or("<no project>".to_string(), |n| n.to_string());
