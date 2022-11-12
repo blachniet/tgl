afad964e39f19a95051fa3f5950aa027ca82b93e
Brian Lachniet <blachniet@gmail.com>
2022-11-12 08:34:41 -0500

    Add svc example

--- examples/svc.rs
+++ examples/svc.rs
@@ -1,10 +1,11 @@
+use chrono::Utc;
 use dialoguer::Confirm;
 use std::env;
-use tgl_cli::api;
+use tgl_cli::svc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = env::var("TOGGL_API_TOKEN").expect("missing TOGGL_API_TOKEN environment variable");
-    let client = api::Client::new(token)?;
+    let client = svc::Client::new(token, Utc::now)?;
     let workspaces = client.get_workspaces()?;
 
     if Confirm::new().with_prompt("Print workspaces?").interact()? {
@@ -12,7 +13,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     for w in workspaces {
-        let projects = client.get_projects(&w.id)?;
+        let projects = client.get_projects(w.id)?;
         let projects: Vec<_> = projects.iter().filter(|p| p.active).collect();
 
         if Confirm::new()
@@ -22,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("{projects:#?}");
         }
 
-        let time_entries = client.get_time_entries(None)?;
+        let time_entries = client.get_latest_entries()?;
 
         if Confirm::new()
             .with_prompt("Print recent time entries?")
